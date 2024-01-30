24663ddb06f42f9b620dfd3006d6797ac92918d9
crywhat <miltonbarrientos2@gmail.com>
2024-01-29 20:46:21 -0600

    log al iniciar

--- api/src/main.ts
+++ api/src/main.ts
@@ -9,6 +9,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3000).then((r) => {
+    console.log(`Server running on port ${process.env.PORT || 3000}`);
+  });
 }
 bootstrap();
