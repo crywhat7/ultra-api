124e6f65464205c2a94a1697db20804152fdfb7a
crywhat <miltonbarrientos2@gmail.com>
2024-03-02 23:30:28 -0600

    xd2

--- frontend/src/app/pages/atm/components/ui-components/button/button.component.ts
+++ frontend/src/app/pages/atm/components/ui-components/button/button.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-button',
-  standalone: true,
-  imports: [],
+  selector: 'atm-button',
   templateUrl: './button.component.html',
-  styleUrl: './button.component.scss'
 })
-export class ButtonComponent {
+export class AtmButtonComponent {
+  @Input() className = '';
+  @Input() disabled = false;
+  @Input() type:
+    | 'primary'
+    | 'secondary'
+    | 'danger'
+    | 'warning'
+    | 'success'
+    | 'info' = 'primary';
 
+  get classes(): string {
+    const base = 'py-2 px-4 rounded-lg justify-center items-center flex gap-3';
+    const stylesClases = {
+      primary: 'bg-atm-500 text-white hover:bg-atm-600',
+      secondary: 'bg-atm-100 text-atm-500 hover:bg-atm-200',
+      danger: 'bg-red-500 text-white hover:bg-red-600',
+      warning: 'bg-yellow-500 text-white hover:bg-yellow-600',
+      success: 'bg-green-500 text-white hover:bg-green-600',
+      info: 'bg-blue-500 text-white hover:bg-blue-600',
+    };
+
+    return ``;
+  }
 }
